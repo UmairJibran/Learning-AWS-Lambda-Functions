54d004dce28a2919f9e01e003c4d6aa71da007fa
Umair Jibran <umairjibran7@gmail.com>
2021-06-24 15:21:05 +0500

    update clock

--- clock/app.js
+++ clock/app.js
@@ -4,11 +4,20 @@ exports.lambdaHandler = async event => {
 	console.log("Clock Route was hit!");
 	console.log({ event });
 
-	const time = moment();
-	const response = {
-		statusCode: 200,
-		body: { message: JSON.stringify(`Current Time is: ${time}`) },
-	};
+	try {
+		const time = moment();
+		const response = {
+			statusCode: 200,
+			body: { message: JSON.stringify(`Current Time is: ${time}`) },
+		};
 
-	return response;
+		return response;
+	} catch (err) {
+		console.error("An Error occurred in clock function", err);
+		const response = {
+			statusCode: 500,
+			body: JSON.stringify(`An Error occurred in clock function`),
+		};
+		return response;
+	}
 };
